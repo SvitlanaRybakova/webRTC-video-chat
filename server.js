91f0a6d8ab35461d2fa3b47a547875e4893ca5d6
Svitlana <devop.edu@gmail.com>
2022-01-28 12:16:53 +0100

    try to render several clients stream

--- server.js
+++ server.js
@@ -1,97 +1,204 @@
+// const path = require("path");
+// const express = require("express");
+// const app = express();
+// const server = require("http").createServer(app);
+// const io = require("socket.io")(server);
+// const ACTIONS = require("./src/socket/actions");
+// const { version, validate } = require("uuid");
+
+// console.log("version************", version);
+// console.log("validate************", validate);
+// const PORT = process.env.PORT || 3001;
+
+// // get all available socket's connection
+// function getClientRooms() {
+//   const { rooms } = io.sockets.adapter;
+
+//   // exclude rooms created by default (returns only these wich user creates themself)
+//   return Array.from(rooms.keys()).filter(
+//     (roomID) => validate(roomID) && version(roomID) === 4
+//   );
+// }
+
+// // if apears a new socket connection, share the information about the with all rooms
+// function shareRoomsInfo() {
+//   io.emit(ACTIONS.SHARE_ROOMS, {
+//     rooms: getClientRooms(),
+//   });
+// }
+// // connect client and server sockets together
+// io.on("connection", (socket) => {
+//   console.log("Socket connected");
+//   // share  info about all available rooms
+//   shareRoomsInfo();
+
+//   // an action in case jf user wants to join room
+//   socket.on(ACTIONS.JOIN, (config) => {
+//     const { room: roomID } = config;
+
+//     // check if the user has been connected earlier
+//     const { rooms: joinedRooms } = socket;
+//     if (Array.from(joinedRooms).includes(roomID)) {
+//       return console.warn("Already joined to room", roomID);
+//     }
+//     // if a user has not been connected, get all clients who are in this room
+//     const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+//     clients.forEach((clientID) => {
+//       // send offer to FIRST client (who is already iside room) this client accept offer(not create it)
+//       io.to(clientID).emit(ACTIONS.ADD_PEER, {
+//         peerID: socket.id,
+//         createOffer: false,
+//       });
+
+//       // the second user who entered the room (create the offer)
+//       socket.emit(ACTIONS.ADD_PEER, {
+//         peerID: clientID,
+//         createOffer: true,
+//       });
+//     });
+
+//     // join the room
+//     socket.join(roomID);
+//     // share information about new room
+//     shareRoomsInfo();
+//   });
+
+//   function leaveRoom() {
+//     // get all rooms
+//     const { rooms } = socket;
+
+//     Array.from(rooms).forEach((roomId) => {
+//       // get all clients in room
+//       const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+
+//       clients.forEach((clientID) => {
+//         // send all clients action to disconnect
+//         io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+//           peerID: socket.id,
+//         });
+//         // disconnect themself
+//         socket.emit(ACTIONS.REMOVE_PEER, {
+//           peerID: clientID,
+//         });
+//       });
+
+//       socket.leave(roomId);
+//     });
+
+//     shareRoomsInfo();
+//   }
+//   // if a user decided to leave video-chat
+//   socket.on(ACTIONS.LEAVE, leaveRoom);
+//   // if socket was emergency disconnected
+//   socket.on("disconnecting", leaveRoom);
+// });
+
+// server.listen(PORT, () => {
+//   console.log("Server Started!");
+// });
 const path = require("path");
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-const ACTIONS = require("./src/socket/actions");
 const { version, validate } = require("uuid");
 
-console.log("version************", version);
-console.log("validate************", validate);
+const ACTIONS = require("./src/socket/actions");
 const PORT = process.env.PORT || 3001;
 
-// get all available socket's connection
 function getClientRooms() {
   const { rooms } = io.sockets.adapter;
 
-  // exclude rooms created by default (returns only these wich user creates themself)
   return Array.from(rooms.keys()).filter(
     (roomID) => validate(roomID) && version(roomID) === 4
   );
 }
 
-// if apears a new socket connection, share the information about the with all rooms
 function shareRoomsInfo() {
   io.emit(ACTIONS.SHARE_ROOMS, {
     rooms: getClientRooms(),
   });
 }
-// connect client and server sockets together
+
 io.on("connection", (socket) => {
-  console.log("Socket connected");
-  // share  info about all available rooms
   shareRoomsInfo();
 
-  // an action in case jf user wants to join room
   socket.on(ACTIONS.JOIN, (config) => {
     const { room: roomID } = config;
-
-    // check if the user has been connected earlier
     const { rooms: joinedRooms } = socket;
+
     if (Array.from(joinedRooms).includes(roomID)) {
-      return console.warn("Already joined to room", roomID);
+      return console.warn(`Already joined to ${roomID}`);
     }
-    // if a user has not been connected, get all clients who are in this room
+
     const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
 
     clients.forEach((clientID) => {
-      // send offer to FIRST client (who is already iside room) this client accept offer(not create it)
       io.to(clientID).emit(ACTIONS.ADD_PEER, {
         peerID: socket.id,
         createOffer: false,
       });
 
-      // the second user who entered the room (create the offer)
       socket.emit(ACTIONS.ADD_PEER, {
         peerID: clientID,
         createOffer: true,
       });
     });
 
-    // join the room
     socket.join(roomID);
-    // share information about new room
     shareRoomsInfo();
   });
 
   function leaveRoom() {
-    // get all rooms
     const { rooms } = socket;
 
-    Array.from(rooms).forEach((roomId) => {
-      // get all clients in room
-      const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    Array.from(rooms)
+      // LEAVE ONLY CLIENT CREATED ROOM
+      .filter((roomID) => validate(roomID) && version(roomID) === 4)
+      .forEach((roomID) => {
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
 
-      clients.forEach((clientID) => {
-        // send all clients action to disconnect
-        io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
-          peerID: socket.id,
+        clients.forEach((clientID) => {
+          io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+            peerID: socket.id,
+          });
+
+          socket.emit(ACTIONS.REMOVE_PEER, {
+            peerID: clientID,
+          });
         });
-        // disconnect themself
-        socket.emit(ACTIONS.REMOVE_PEER, {
-          peerID: clientID,
-        });
+
+        socket.leave(roomID);
       });
-
-      socket.leave(roomId);
-    });
 
     shareRoomsInfo();
   }
-  // if a user decided to leave video-chat
+
   socket.on(ACTIONS.LEAVE, leaveRoom);
-  // if socket was emergency disconnected
   socket.on("disconnecting", leaveRoom);
+
+  socket.on(ACTIONS.RELAY_SDP, ({ peerID, sessionDescription }) => {
+    io.to(peerID).emit(ACTIONS.SESSION_DESCRIPTION, {
+      peerID: socket.id,
+      sessionDescription,
+    });
+  });
+
+  socket.on(ACTIONS.RELAY_ICE, ({ peerID, iceCandidate }) => {
+    io.to(peerID).emit(ACTIONS.ICE_CANDIDATE, {
+      peerID: socket.id,
+      iceCandidate,
+    });
+  });
+});
+
+const publicPath = path.join(__dirname, "build");
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 server.listen(PORT, () => {
--- src/hooks/useWebRTC.js
+++ src/hooks/useWebRTC.js
@@ -1,5 +1,7 @@
-import {useRef, useCallback, useEffect} from 'react'
+import { useRef, useCallback, useEffect } from "react";
 import socket from "../socket";
+// provide a list with free stun servers adress
+import freeice from "freeice";
 import ACTIONS from "../socket/actions";
 import useStateWithCallback from "./useStateWithCallback";
 
@@ -11,7 +13,7 @@ export default function useWebRTC(roomId) {
   const peerConnections = useRef({});
   const localMediaStream = useRef(null);
   const peerMediaElements = useRef({
-    [LOCAL_VIDEO] : null,
+    [LOCAL_VIDEO]: null,
   });
 
   // check clients for the unique value
@@ -28,6 +30,145 @@ export default function useWebRTC(roomId) {
     },
     [clients, updateClients]
   );
+ 
+  // create answer 
+  useEffect(() => {
+    async function setRemoteMedia({
+      peerID,
+      sessionDescription: remoteDescription,
+    }) {
+      await peerConnections.current[peerID]?.setRemoteDescription(
+        new RTCSessionDescription(remoteDescription)
+      );
+
+      if (remoteDescription.type === "offer") {
+        const answer = await peerConnections.current[peerID].createAnswer();
+
+        await peerConnections.current[peerID].setLocalDescription(answer);
+
+        socket.emit(ACTIONS.RELAY_SDP, {
+          peerID,
+          sessionDescription: answer,
+        });
+      }
+    }
+
+    socket.on(ACTIONS.SESSION_DESCRIPTION, setRemoteMedia);
+
+    return () => {
+      socket.off(ACTIONS.SESSION_DESCRIPTION);
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on(ACTIONS.ICE_CANDIDATE, ({ peerID, iceCandidate }) => {
+      peerConnections.current[peerID]?.addIceCandidate(
+        new RTCIceCandidate(iceCandidate)
+      );
+    });
+
+    return () => {
+      socket.off(ACTIONS.ICE_CANDIDATE);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleRemovePeer = ({ peerID }) => {
+      if (peerConnections.current[peerID]) {
+        peerConnections.current[peerID].close();
+      }
+
+      delete peerConnections.current[peerID];
+      delete peerMediaElements.current[peerID];
+
+      updateClients((list) => list.filter((c) => c !== peerID));
+    };
+
+    socket.on(ACTIONS.REMOVE_PEER, handleRemovePeer);
+
+    return () => {
+      socket.off(ACTIONS.REMOVE_PEER);
+    };
+  }, []);
+
+  useEffect(() => {
+    async function handleNewPeer({ peerID, createOffer }) {
+      // check if we already connected to current peer
+      if (peerID in peerConnections.current) {
+        return console.warn(`Already connected to peer ${peerID}`);
+      }
+      // create a new peer connection
+      peerConnections.current[peerID] = new RTCPeerConnection({
+        // adresses a free stun servers
+        iceServers: freeice(),
+      });
+
+      peerConnections.current[peerID].onicecandidate = (event) => {
+        // if we want to create a room (event===candidate) = share our offer to other clients
+        if (event.candidate) {
+          socket.emit(ACTIONS.RELAY_ICE, {
+            peerID,
+            iceCandidate: event.candidate,
+          });
+        }
+      };
+
+      let tracksNumber = 0;
+
+      peerConnections.current[peerID].ontrack = ({
+        streams: [remoteStream],
+      }) => {
+        tracksNumber++;
+        // check that we got a 2 tracks (video & audio)
+        if (tracksNumber === 2) {
+          tracksNumber = 0;
+          //  extracts streams what we got from other users
+          addNewClient(peerID, () => {
+            if (peerMediaElements.current[peerID]) {
+              peerMediaElements.current[peerID].srcObject = remoteStream;
+            } else {
+              // FIX LONG RENDER IN CASE OF MANY CLIENTS
+              let settled = false;
+              const interval = setInterval(() => {
+                if (peerMediaElements.current[peerID]) {
+                  peerMediaElements.current[peerID].srcObject = remoteStream;
+                  settled = true;
+                }
+
+                if (settled) {
+                  clearInterval(interval);
+                }
+              }, 1000);
+            }
+          });
+        }
+      };
+      // add localStream to peer connection
+      localMediaStream.current.getTracks().forEach((track) => {
+        peerConnections.current[peerID].addTrack(
+          track,
+          localMediaStream.current
+        );
+      });
+
+      if (createOffer) {
+        const offer = await peerConnections.current[peerID].createOffer();
+
+        await peerConnections.current[peerID].setLocalDescription(offer);
+
+        socket.emit(ACTIONS.RELAY_SDP, {
+          peerID,
+          sessionDescription: offer,
+        });
+      }
+    }
+
+    socket.on(ACTIONS.ADD_PEER, handleNewPeer);
+
+    return () => {
+      socket.off(ACTIONS.ADD_PEER);
+    };
+  }, []);
 
   useEffect(() => {
     async function startCapture() {
@@ -40,11 +181,11 @@ export default function useWebRTC(roomId) {
       });
       // if we got video stream from current screen
       addNewClient(LOCAL_VIDEO, () => {
-        const localVideoElement = peerMediaElements.current[LOCAL_VIDEO]
+        const localVideoElement = peerMediaElements.current[LOCAL_VIDEO];
 
-        if(localVideoElement){
+        if (localVideoElement) {
           localVideoElement.volum = 0;
-          localVideoElement.srcObject = localMediaStream.current
+          localVideoElement.srcObject = localMediaStream.current;
         }
       });
     }
@@ -53,14 +194,18 @@ export default function useWebRTC(roomId) {
     startCapture()
       .then(() => socket.emit(ACTIONS.JOIN, { room: roomId }))
       .catch((e) => console.error("Error getting userMedia:", e));
+
+    // logout from room if the component has been unmounted
+    return () => {
+      localMediaStream.current.getTracks().forEach((track) => track.stop());
+
+      socket.emit(ACTIONS.LEAVE);
+    };
   }, [roomId]);
 
   // ref for video node (src)
-  const provideMediaRef = useCallback(
-    (id, node) => {
-      peerMediaElements.current[id] = node
-    },
-    [],
-  )
-   return { clients, provideMediaRef };
+  const provideMediaRef = useCallback((id, node) => {
+    peerMediaElements.current[id] = node;
+  }, []);
+  return { clients, provideMediaRef };
 }
